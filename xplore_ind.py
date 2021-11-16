d58bc215df635f3a8119da1465bf1bb714e766ac
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2021-11-16 21:48:52 +0100

    Updated xplore_ind to the new structure with config and configurable individual

--- xplore_ind.py
+++ xplore_ind.py
@@ -2,10 +2,13 @@ import time
 
 from modbots.evaluate.sideChannelPythonside import SideChannelPythonside
 from modbots.evaluate import get_env, evaluate, close_env, set_env_variables
-from modbots.creature_types.central_ctrnn_ind import Individual
+from modbots.creature_types.configurable_individual import Individual
 
 import argparse
 import re
+
+from config_util import get_local_config
+from localconfig import config
 
 """def get_type(gene):
     float_pat = rf""
@@ -14,28 +17,36 @@ import re
 # Add arguments
 parser = argparse.ArgumentParser(description='Explore some boys')
 parser.add_argument(
-    'config_file',
-    type=str,
-    help='The config file to configure this evolution'
+    '--config_file',
+    type = str,
+    help='The config file that was used with thiss individual',
+    default=get_local_config()
 )
 parser.add_argument(
-    'gene',
-    type=str,
-    help='The gene to evaluate'
+    '--gene',
+    type = str,
+    help='The gene file',
+    default="bestInd/ind"
 )
 
 args = parser.parse_args()
-with open(args.config_file, "r") as file:
-    for line in file:
-        exec(line)
-        print(line)
 
-#ind_type = get_type(args.gene)
+config.read(args.config_file)
 
 print("We start")
 start = time.time()
-ind = Individual(args.gene)
-set_env_variables(PATH, LOG_FOLDER, seed=SEED, headless=HEADLESS, n_steps=N_STEPS, n_start_eval=N_START_EVAL)
+ind = Individual.unpack_ind(args.gene, config)
+
+set_env_variables(
+    config.files.build_path,
+    config.files.log_folder,
+    seed=config.experiment.seed,
+    headless=config.experiment.headless,
+    n_steps=config.evaluation.n_steps,
+    n_start_eval=config.evaluation.n_start_eval,
+    time_scale=config.evaluation.time_scale
+)
+
 print("Starting:", time.time()-start)
 
 start = time.time()
