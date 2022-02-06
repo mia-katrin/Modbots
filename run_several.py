7615de511b1f236be2ea2befe9dd28f09ab184c7
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2022-02-07 00:40:02 +0100

    Trying to fix issue

--- run_several.py
+++ run_several.py
@@ -71,7 +71,14 @@ def run_on_config(conf_name):
     config.filename = conf_name
 
     for _ in range(INTERNAL_ROUNDS):
-        evolve(config, run_label, show_figs=False, runNr=runNr)
+        failed = 0
+        try_again = True
+        while failed < 10 and try_again:
+            try:
+                evolve(config, run_label, show_figs=False, runNr=runNr)
+                try_again = False
+            except:
+                failed += 1
         append_runNr(conf_name)
         runNr += 1
 
