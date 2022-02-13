9255844f82599ca9be4097b0f9f609cdef237b12
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2022-02-13 14:04:12 +0100

    New run

--- make_experiment_configs.py
+++ make_experiment_configs.py
@@ -13,7 +13,7 @@ with open("experiments/max_cores.txt") as file:
 cs = [0.01,0.08,0.16,0.24,0.32,0.48]
 bs = [0.01,0.01,0.01,0.01,0.01,0.01]
 
-mode = "gradual"
+mode = ""
 brain = "dec_ctrnn"
 
 ########## COPY ##########
