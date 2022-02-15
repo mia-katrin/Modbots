021d0206825d750cea00d1721580f354db71084d
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2022-02-15 21:44:19 +0100

    A way to make batch script making easier

---
+++ sbatch_make.py
@@ -0,0 +1,56 @@
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--brain", type=str)
+parser.add_argument("-m", "--mode", type=str)
+
+args = parser.parse_args()
+brain = args.brain.title()
+mode = args.mode.title()
+
+label = f"{brain}, {mode}: Tuning " # and then a number
+highest_tune_nr = 0
+
+valid_intervals = None
+with open("experiments/valid_intervals", "r") as file:
+    valid_intervals = json.load(file)
+
+for exp_label in valid_intervals.keys():
+    if exp_label.startswith(label):
+        number = exp_label.removeprefix(label)
+        number = int(number)
+
+        if number > highest_tune_nr:
+            highest_tune_nr = number
+
+label += str(highest_tune_nr)
+
+jobname = brain[0] + mode[0] + mode[2] + "Tun" + str(highest_tune_nr)
+
+with open("tune_job.sh", "w") as file:
+    file.write(f"""#!/bin/bash
+
+#SBATCH --job-name={jobname}
+
+#SBATCH --account=ec29
+
+#SBATCH --partition=normal
+
+#SBATCH --time=30:00:00
+
+#SBATCH --ntasks=1
+
+#SBATCH --cpus-per-task=25
+
+#SBATCH --mem-per-cpu=1G
+
+set -o errexit
+set -o nounset
+
+source /fp/homes01/u01/ec-mkkvalsu/evolve_unity_env/bin/activate
+
+srun python3 run_several.py -l \"{label}\"
+""")
+
+print("File made")
