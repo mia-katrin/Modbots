d706545c7836e4eaeaeb9e312fd709f3745010ba
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2022-02-16 00:14:45 +0100

    New run

--- make_experiment_configs.py
+++ make_experiment_configs.py
@@ -10,8 +10,8 @@ with open("experiments/max_cores.txt") as file:
 
 ########## COPY ##########
 
-cs = [0.32,0.32,0.24]
-bs = [0.48,0.64,0.64]
+cs = [0.16,0.24,0.32,0.48]
+bs = [0.01,0.01,0.01,0.01]
 
 mode = ""
 brain = "copy"
