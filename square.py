54c0a32237d3e1d9aa1fb2b5d5de0d07a1e2aa50
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2022-02-17 12:25:09 +0100

    Shit forgot it

--- square.py
+++ square.py
@@ -10,6 +10,26 @@ import matplotlib.pyplot as plt
 
 config_pattern = re.compile("[0-9]+c[0-9]+b.*\.cfg$")
 
+def boxplot(data, edge_color, fill_color, labels, ax):
+    bp = ax.boxplot(data, patch_artist=True, showmeans=True, labels=labels)
+
+    for i, patch in enumerate(bp['boxes']):
+        patch.set(color=edge_color[i])
+    for i, patch in enumerate(bp['boxes']):
+        patch.set(facecolor=fill_color[i])
+
+    for i, patch in enumerate(bp['medians']):
+        patch.set(color=edge_color[i])
+
+    for i, patch in enumerate(bp['caps']):
+        patch.set(color=edge_color[i//2])
+    for i, patch in enumerate(bp['whiskers']):
+        patch.set(color=edge_color[i//2])
+    for i, patch in enumerate(bp['fliers']):
+        patch.set(color=edge_color[i])
+
+    return bp
+
 class SquareManager:
     def __init__(self):
         """self.squares = {
@@ -71,28 +91,141 @@ class SquareManager:
         )
         return True
 
-    def print_all(self):
+    def print_all(self, runs=False):
         print()
         for brain_type in self.squares.keys():
             for mode in self.squares[brain_type].keys():
                 print(brain_type.title() if brain_type != "" else "Sine", mode.title(),"\n")
-                self.squares[brain_type][mode].print_fitnesses()
+                if runs:
+                    self.squares[brain_type][mode].print_nr_runs()
+                else:
+                    self.squares[brain_type][mode].print_fitnesses()
 
-    def plot_all(self):
-        full_matrix = np.zeros((8*4,8*4))
+    def plot_all(self, runs=False):
         for i, brain_type in enumerate(self.squares.keys()):
-            for j, mode in enumerate(self.squares[brain_type].keys()):
-                matrix, cs, bs = self.squares[brain_type][mode].get_fitness_matrix()
-                full_matrix[i*8:i*8+len(cs),j*8:j*8+len(bs)] = matrix
+            fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex = True, sharey = False)
+            fig.suptitle(brain_type.title())
 
-        fig1, ax1 = plt.subplots(1, sharex = True, sharey = False)
-        ax1.imshow(full_matrix.astype(int))
-        for i, row in enumerate(full_matrix):
-            for j, val in enumerate(row):
-                if val != 0:
-                    ax1.text(j, i, round(val), color='black', ha='center', va='center')
+            subplot_axes = (ax1, ax2, ax3, ax4)
+
+            for j, mode in enumerate(self.squares[brain_type].keys()):
+                ax = subplot_axes[j]
+                ax.title.set_text(mode.title() if mode != "" else "Normal")
+
+                if runs:
+                    matrix, cs, bs = self.squares[brain_type][mode].get_nr_runs_matrix()
+                else:
+                    matrix, cs, bs = self.squares[brain_type][mode].get_fitness_matrix()
+
+                ax.imshow(matrix.astype(int), vmin=0, vmax=30 if not runs else 4)
+                plt.xticks(list(range(len(bs))), bs)
+                plt.yticks(list(range(len(cs))), cs)
+                for i, row in enumerate(matrix):
+                    for j, val in enumerate(row):
+                        if val != 0:
+                            ax.text(j, i, round(val), color='black', ha='center', va='center')
 
         plt.show()
+
+    def plot_all_collapsed(self):
+        for brain_type in self.squares.keys():
+            for mode in self.squares[brain_type].keys():
+                square = self.squares[brain_type][mode]
+                self.plot_collapsed(square, brain_type, mode)
+        plt.show()
+
+    def plot_collapsed(self, square, brain_type, mode):
+        cs = square.get_cs()
+        bs = square.get_bs()
+
+        ### B collapsed
+
+        b_lines = {}
+        for b in bs:
+            b_lines[b] = []
+
+        for c in cs:
+            for b in square.runs[c].keys():
+                lines = square.get_stat_lines(c, b)
+                if len(lines) > 0:
+                    for line in lines:
+                        b_lines[b].append(line)
+
+        colors = plt.cm.viridis(np.linspace(0, 1, len(bs)))
+        transparents = []
+        for color in colors:
+            transparents.append([color[0], color[1], color[2], 0.3])
+
+        fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2, sharex = False, sharey = False)
+        fig.suptitle(brain_type.title() + " " + mode.title())
+
+        ax1.set_xlabel("Generation")
+        ax1.set_ylabel("Fitness")
+        ax1.title.set_text("Body mut rates plotted for all control mut rates")
+
+        for i, b in enumerate(b_lines.keys()):
+            for line in b_lines[b]:
+                ax1.plot(line, color=transparents[i])
+
+        for i, b in enumerate(b_lines.keys()):
+            if len(b_lines[b]) > 0:
+                ax1.plot(np.sum(b_lines[b], axis=0)/len(b_lines[b]), color=colors[i], linewidth=2)
+
+        ### B collapsed boxplot
+
+        all_last_values = []
+        for b, lines in b_lines.items():
+            all_last_values.append([])
+            for line in lines:
+                all_last_values[-1].append(line[-1])
+
+        boxplot(all_last_values, colors, transparents, [str(b) for b in bs], ax2)
+        ax2.set_ylabel("Fitness")
+        ax2.set_xlabel("Body mutation rate")
+        ax2.title.set_text("Body mut rates plotted for all control mut rates")
+
+        ### C collapsed
+
+        c_lines = {}
+        for c in cs:
+            c_lines[c] = []
+
+        for c in cs:
+            for b in square.runs[c].keys():
+                lines = square.get_stat_lines(c, b)
+                if len(lines) > 0:
+                    for line in lines:
+                        c_lines[c].append(line)
+
+        colors = plt.cm.viridis(np.linspace(0, 1, len(cs)))
+        transparents = []
+        for color in colors:
+            transparents.append([color[0], color[1], color[2], 0.3])
+
+        ax3.set_xlabel("Generation")
+        ax3.set_ylabel("Fitness")
+        ax3.title.set_text("Control mut rates plotted for all body mut rates")
+
+        for i, c in enumerate(c_lines.keys()):
+            for line in c_lines[c]:
+                ax3.plot(line, color=transparents[i])
+
+        for i, c in enumerate(c_lines.keys()):
+            if len(c_lines[c]) > 0:
+                ax3.plot(np.sum(c_lines[c], axis=0)/len(c_lines[c]), color=colors[i], linewidth=2)
+
+        ### C collapsed boxplot
+
+        all_last_values = []
+        for c, lines in c_lines.items():
+            all_last_values.append([])
+            for line in lines:
+                all_last_values[-1].append(line[-1])
+
+        boxplot(all_last_values, colors, transparents, [str(c) for c in cs], ax4)
+        ax4.set_ylabel("Fitness")
+        ax4.set_xlabel("Control mutation rate")
+        ax4.title.set_text("Control mut rates plotted for all body mut rates")
 
 class Square:
     def __init__(self, experiment_folder: str):
@@ -105,6 +238,7 @@ class Square:
         self.print_avg_mutated = self.treat_spots(self.internal_avg_mutated_spot)
 
         self.get_fitness_matrix = self.get_matrix_getter(self.get_fitness)
+        self.get_nr_runs_matrix = self.get_matrix_getter(self.get_valid_runs)
 
     def add_run(self, runNr, c, b):
         if c not in self.runs.keys():
@@ -114,6 +248,19 @@ class Square:
             self.runs[c][b] = []
 
         self.runs[c][b].append(runNr)
+
+    def get_stat_lines(self, c, b):
+        lines = []
+
+        for runNr in self.runs[c][b]:
+            path = f"{self.experiment_folder}/run{runNr}"
+            try:
+                with open(path+"/data", "rb") as file:
+                    data = pickle.load(file)
+                    lines.append(data["Fitness"]["Maxs"])
+            except:
+                pass
+        return lines
 
     def get_cs(self):
         cs = []
