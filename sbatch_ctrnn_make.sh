3609484bb950952af106de295e72e148c25fca8a
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2022-02-21 00:13:49 +0100

    Putting on the rest of the ctrnn runs

--- sbatch_ctrnn_make.sh
+++ sbatch_ctrnn_make.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for mode in normal
+for mode in normal variable growing gradual
 do
   echo $mode
   for c in 0.01 0.02 0.04 0.08 0.16 0.24 0.32 0.48
   do
     #python3 make_experiment_configs.py -m $mode -br dec_ctrnn_sine -c $c $c $c $c $c $c $c $c -b 0.01 0.08 0.16 0.24 0.32 0.48 0.64 0.82
-    python3 sbatch_make.py -m $mode -br dec_ctrnn_sine -c $c $c $c $c $c $c $c $c -b 0.01 0.08 0.16 0.24 0.32 0.48 0.64 0.82
+    python3 sbatch_make.py -m $mode -br cen_ctrnn -c $c $c $c $c -b 0.48 0.64 0.82 0.24
     sleep 1
     sbatch tune_job.sh
     sleep 5
