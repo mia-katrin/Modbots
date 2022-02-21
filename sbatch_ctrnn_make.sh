120a5bfc3a2ea4c950c01c3be5a3e52cb0fff256
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2022-02-21 18:56:09 +0100

    Script to run copy_sine on fox

--- sbatch_ctrnn_make.sh
+++ sbatch_ctrnn_make.sh
@@ -5,18 +5,12 @@ do
   echo $mode
   for c in 0.01 0.02 0.04 0.08 0.16 0.24 0.32 0.48
   do
-    #python3 make_experiment_configs.py -m $mode -br dec_ctrnn_sine -c $c $c $c $c $c $c $c $c -b 0.01 0.08 0.16 0.24 0.32 0.48 0.64 0.82
-    python3 sbatch_make.py -m $mode -br cen_ctrnn -c $c $c $c $c -b 0.48 0.64 0.82 0.24
+    python3 make_experiment_configs.py -m $mode -br copy_sine -c $c $c $c $c $c $c $c $c -b 0.01 0.08 0.16 0.24 0.32 0.48 0.64 0.82
+    python3 sbatch_make.py -m $mode -br copy_sine -c $c $c $c $c $c $c $c $c -b 0.01 0.08 0.16 0.24 0.32 0.48 0.64 0.82
     sleep 1
     sbatch tune_job.sh
     sleep 5
   done
 done
-
-#python3 run_several.py -l "Variable sine fill 7" -m variable -br copy -c 0.01 0.02 -b 0.48 0.64
-
-#python3 sbatch_make.py -br cen_ctrnn -m variable -c 0.01 0.02 -b 0.48 0.64
-
-#sbatch tune_job.sh
 
 #python3 run_several.py -l "Variable sine fill 7" -m variable -br sine -c 0.16 0.16 0.16 0.16 -b 0.01 0.08 0.16 0.24
