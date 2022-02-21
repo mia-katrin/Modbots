e39641f5e5372b6912550bd5c40a6816dc9914ce
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2022-02-21 19:37:51 +0100

    Going for the first square on sine copy to see if it is worth it

--- sbatch_ctrnn_make.sh
+++ sbatch_ctrnn_make.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-for mode in normal variable growing gradual
+for mode in normal
 do
   echo $mode
   for c in 0.01 0.02 0.04 0.08 0.16 0.24 0.32 0.48
   do
-    python3 make_experiment_configs.py -m $mode -br copy_sine -c $c $c $c $c $c $c $c $c -b 0.01 0.08 0.16 0.24 0.32 0.48 0.64 0.82
-    python3 sbatch_make.py -m $mode -br copy_sine -c $c $c $c $c $c $c $c $c -b 0.01 0.08 0.16 0.24 0.32 0.48 0.64 0.82
-    sleep 1
-    sbatch tune_job.sh
-    sleep 5
+    for vs in 1 2 3 4
+    do
+      if [ $vs -eq 1 ]
+      then
+        python3 make_experiment_configs.py -m $mode -br copy_sine -c $c $c -b 0.01 0.08
+        python3 sbatch_make.py -m $mode -br copy_sine -c $c $c -b 0.01 0.08
+      elif [ $vs -eq 2 ]
+      then
+        python3 make_experiment_configs.py -m $mode -br copy_sine -c $c $c -b 0.16 0.24
+        python3 sbatch_make.py -m $mode -br copy_sine -c $c $c -b 0.16 0.24
+      elif [ $vs -eq 2 ]
+      then
+        python3 make_experiment_configs.py -m $mode -br copy_sine -c $c $c -b 0.32 0.48
+        python3 sbatch_make.py -m $mode -br copy_sine -c $c $c-b 0.32 0.48
+      elif [ $vs -eq 2 ]
+      then
+        python3 make_experiment_configs.py -m $mode -br copy_sine -c $c $c-b 0.64 0.82
+        python3 sbatch_make.py -m $mode -br copy_sine -c $c $c -b 0.64 0.82
+      else
+        echo "Please kill me"
+      fi
+      sleep 1
+      sbatch tune_job.sh
+      sleep 5
+    done
   done
 done
 
