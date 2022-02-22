4985ac433470fd7e9409a27c300dc0b966dc7b5f
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2022-02-22 01:16:46 +0100

    Ready to sweep for copy_sine

--- sbatch_ctrnn_make.sh
+++ sbatch_ctrnn_make.sh
@@ -1,36 +1,23 @@
 #!/bin/bash
 
-for mode in normal
+for mode in variable growing gradual
 do
   echo $mode
   for c in 0.01 0.02 0.04 0.08 0.16 0.24 0.32 0.48
   do
-    for vs in 1 2 3 4
-    do
-      if [ $vs -eq 1 ]
-      then
-        python3 make_experiment_configs.py -m $mode -br copy_sine -c $c $c -b 0.01 0.08
-        python3 sbatch_make.py -m $mode -br copy_sine -c $c $c -b 0.01 0.08 -i 2
-      elif [ $vs -eq 2 ]
-      then
-        python3 make_experiment_configs.py -m $mode -br copy_sine -c $c $c -b 0.16 0.24
-        python3 sbatch_make.py -m $mode -br copy_sine -c $c $c -b 0.16 0.24 -i 2
-      elif [ $vs -eq 3 ]
-      then
-        python3 make_experiment_configs.py -m $mode -br copy_sine -c $c $c -b 0.32 0.48
-        python3 sbatch_make.py -m $mode -br copy_sine -c $c $c -b 0.32 0.48 -i 2
-      elif [ $vs -eq 4 ]
-      then
-        python3 make_experiment_configs.py -m $mode -br copy_sine -c $c $c -b 0.64 0.82
-        python3 sbatch_make.py -m $mode -br copy_sine -c $c $c -b 0.64 0.82 -i 2
-      else
-        echo "Please kill me"
-      fi
-      sleep 1
-      sbatch tune_job.sh
-      sleep 5
-    done
+    python3 make_experiment_configs.py -m $mode -br copy_sine -c $c $c $c $c $c $c $c $c -b 0.01 0.08 0.16 0.24 0.32 0.48 0.64 0.82
+    python3 sbatch_make.py -m $mode -br copy_sine -c $c $c $c $c $c $c $c $c -b 0.01 0.08 0.16 0.24 0.32 0.48 0.64 0.82
+    sleep 1
+    sbatch tune_job.sh
+    sleep 5
   done
 done
 
-#python3 run_several.py -l "Variable sine fill 7" -m variable -br sine -c 0.16 0.16 0.16 0.16 -b 0.01 0.08 0.16 0.24
+##### HEY!!!
+
+#
+# For dec_ctrnn_sine, you must have 0.01 0.08 0.16 0.24 0.32 0.48 0.64 0.82 for control
+# And normal variable growing gradual
+#
+
+##### HEY!!!!
