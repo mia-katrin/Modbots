78610f2c3cc3f2e5acef7702415194fff73b1b25
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2022-03-30 01:56:23 +0200

    Raise exception for error

--- evolve.py
+++ evolve.py
@@ -174,16 +174,7 @@ def evolve(config, statement=None, show_figs=True, runNr=None):
                     damaged = True
 
             if damaged:
-                pool.close()
-                pool = multiprocessing.Pool(config.experiment.n_cores)
-                cs = int(np.ceil(float(config.ea.pop_size)/float(config.experiment.n_cores)))
-                toolbox.register("map", pool.map, chunksize=cs)
-
-                fitnesses = toolbox.map(toolbox.evaluate, offspring)
-                for ind, fit in zip(offspring, fitnesses):
-                    ind.fitness = fit
-                    if fit == 0.0:
-                        print("It did not solve the issue of fit=0.0")
+                raise Exception("Fitness should never be a pure 0")
 
             population = offspring
 
