a9c2ce76f96dee9d17a14d4ef632f7aad84269fd
mkkvalsu <mkkvalsu@student.matnat.uio.no>
2022-04-11 21:14:44 +0200

    SideChannel can accept different messages and do not print at message

--- modbots/modbots/evaluate/sideChannelPythonside.py
+++ modbots/modbots/evaluate/sideChannelPythonside.py
@@ -4,21 +4,25 @@ from mlagents_envs.side_channel.side_channel import (
     OutgoingMessage,
 )
 import uuid
+import ast
 
 class SideChannelPythonside(SideChannel):
     def __init__(self) -> None:
         super().__init__(uuid.UUID("621f0a70-4f87-11ea-a6bf-784f4387d1f7"))
         self.created_modules = []
+        self.coordinates = []
 
     def on_message_received(self, msg: IncomingMessage) -> None:
         recieved = msg.read_string()
-        print("[Unity]:", recieved)
+        #print("[Unity]:", recieved)
 
         # If we recieved created modules
         if "Created modules: " in recieved:
-            self.created_modules = []
-            for index in recieved[len("Created modules: "):].split(",")[:-1]:
-                self.created_modules.append(int(index))
+            self.created_modules = ast.literal_eval(recieved[len("Created modules: "):])
+
+        # If we recieved coordinates
+        if recieved.startswith("Coordinates: "):
+            self.coordinates = ast.literal_eval(recieved[len("Coordinates: "):])
 
     def send_string(self, data: str) -> None:
         #print("[Python]: Sending data")
